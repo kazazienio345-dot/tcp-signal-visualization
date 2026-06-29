0abfa0a1845befa1362e3fc237a8d4583015f8ee
Joseph <josephuyana@gmail.com>
2026-06-29 11:50:14 +0200

    Labels and buttons created

--- views/mainView.py
+++ views/mainView.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QWidget
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
 
 
 class MainWindow(QMainWindow):
@@ -8,3 +8,24 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("TCP Signal Visualizer")
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+
+        self.port_label = QLabel("Port:")
+        self.line_edit = QLineEdit()
+        self.line_edit.setPlaceholderText("Enter port")
+        self.connect_button = QPushButton("Connect")
+        self.disconnect_button = QPushButton("Disconnect")
+
+
+        self.layout.addWidget(self.port_label)
+        self.layout.addWidget(self.line_edit)
+        self.layout.addWidget(self.connect_button)
+        self.layout.addWidget(self.disconnect_button)
+
+        self.connect_button.clicked.connect(self.on_connect)
+
+    def on_connect(self):
+        port = self.line_edit.text()
+        print(port)
